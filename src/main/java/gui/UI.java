96691f6b38806d288f7e3bfb69bd5bfb8b0fdf57
Hoyeon Lee <hoyeon.lee@student.uni-tuebingen.de>
2021-07-26 23:52:58 +0200

    final

--- src/main/java/gui/UI.java
+++ src/main/java/gui/UI.java
@@ -49,7 +49,6 @@ public class UI {
      */
     UI() {
 
-
         // Color
         Color inputC = new Color(223, 187, 187); //pale Chestnut
         Color outputC = new Color(249, 241, 214); //antique White
@@ -61,11 +60,12 @@ public class UI {
         Font contentFont = new Font("Courier", Font.PLAIN, 20);
         Font buttonFont = new Font("Courier", Font.BOLD, 20);
 
+        //make the window and set size + background colour
         frame = new JFrame("Kwic UI");
         frame.setSize(1400, 800);
         frame.getContentPane().setBackground(backgroundC);
 
-        //Connect List and SelectionListener
+        //set size
         Dimension size = new Dimension(150, 30);
 
         //Create Buttons
@@ -93,21 +93,25 @@ public class UI {
 
         //Panel1-----------------------------------------------------
 
+        //initialize the first panel
         JPanel panel1 = new JPanel();
         panel1.setBackground(buttonC);
 
-        //Box: "enter URL"
+        //make Textboxes
+        //Text infront of the "Enter the URL" box
         urlText = new JTextArea("URL : ");
         urlText.setMaximumSize(new Dimension(200, 30));
         urlText.setBackground(backgroundC);
         urlText.setEditable(false);
         urlText.setFont(contentFont);
 
+        //Box: "Enter the URL"
         urlField = new JTextField(20);
         urlField.setMaximumSize(new Dimension(400, 30));
         urlField.setText("Enter the URL");
         urlField.setFont(fieldFont);
         urlField.setBackground(inputC);
+        //making text in box disappear when clicked
         urlField.addMouseListener(new MouseAdapter(){
             @Override
             public void mouseClicked(MouseEvent e){
@@ -115,18 +119,20 @@ public class UI {
             }
         });
 
-        //Box: "enter Word"
+        //Text in front of the "enter a word" box
         wordText = new JTextArea("Word : ");
         wordText.setMaximumSize(new Dimension(100, 30));
         wordText.setBackground(backgroundC);
         wordText.setEditable(false);
         wordText.setFont(contentFont);
 
+        //Box: "Enter a word"
         wordField = new JTextField(10);
         wordField.setMaximumSize(new Dimension(400, 30));
         wordField.setText("Enter a word");
         wordField.setFont(fieldFont);
         wordField.setBackground(inputC);
+        //making text in box disappear when clicked
         wordField.addMouseListener(new MouseAdapter(){
             @Override
             public void mouseClicked(MouseEvent e){
@@ -134,18 +140,20 @@ public class UI {
             }
         });
 
-        //Box: "Surrounding words"
+        //Text in front of "surrounding words" box
         surroundingNText = new JTextArea("Surrounding words : ");
         surroundingNText.setMaximumSize(new Dimension(100, 30));
         surroundingNText.setBackground(backgroundC);
         surroundingNText.setEditable(false);
         surroundingNText.setFont(contentFont);
 
+        //Box: "Surrounding words"
         surroundNfield = new JTextField(25);
         surroundNfield.setMaximumSize(new Dimension(200, 30));
         surroundNfield.setText("Enter the number of surrounding words");
         surroundNfield.setFont(fieldFont);
         surroundNfield.setBackground(inputC);
+        //making text in box disappear when clicked
         surroundNfield.addMouseListener(new MouseAdapter(){
             @Override
             public void mouseClicked(MouseEvent e){
@@ -159,24 +167,31 @@ public class UI {
         makeChoice.setFont(fieldFont);
         makeChoice.setBackground(inputC);
 
-        //Override JFrames default layout manager.
+        //Add all the boxes and buttons to panel 1
         BoxLayout aBoxLayout1 = new BoxLayout(panel1, BoxLayout.X_AXIS);
         panel1.setLayout(aBoxLayout1);
+        //Add URL Text + Box
         panel1.add(Box.createRigidArea(new Dimension(20, 0)));
         panel1.add(urlText);
         panel1.add(urlField);
+        //Add Word Text + Box
         panel1.add(Box.createRigidArea(new Dimension(10, 0)));
         panel1.add(wordText);
         panel1.add(wordField);
+        //Add Surrounding Text + Box
         panel1.add(Box.createRigidArea(new Dimension(10, 0)));
         panel1.add(surroundingNText);
         panel1.add(surroundNfield);
+        //Add the search Button
         panel1.add(Box.createRigidArea(new Dimension(10, 0)));
         panel1.add(searchButton);
+        //Add the make choice box
         panel1.add(Box.createRigidArea(new Dimension(10, 0)));
         panel1.add(makeChoice);
+        //add background colour
         panel1.add(Box.createRigidArea(new Dimension(20, 0)));
         panel1.setBackground(backgroundC);
+        //Center all the added boxes and buttons
         panel1.setAlignmentX(Component.CENTER_ALIGNMENT);
 
 
@@ -190,6 +205,7 @@ public class UI {
         sentenceField.setMaximumSize(new Dimension(500, 300));
 
         //output Textbox: "Result"
+        //make sure it can be scrolled through the results
         JScrollPane textAreasp = new JScrollPane(sentenceField);
         textAreasp.setSize(new Dimension(500, 300));
         resultField = new JTextArea("\t \t  ----- Result ----- \t \t\n");
@@ -197,13 +213,13 @@ public class UI {
         resultField.setEditable(false);
         resultField.setFont(contentFont);
 
+        //initialize the second panel
         JPanel panel2 = new JPanel();
-        //Override JFrames default layout manager.
         BoxLayout aBoxLayout = new BoxLayout(panel2, BoxLayout.X_AXIS);
         panel2.setLayout(aBoxLayout);
         panel2.setBackground(backgroundC);
 
-        //Add 5 Pixels rigid space between components
+        //Create rigid space between components
         panel2.add(Box.createRigidArea(new Dimension(20, 20)));
         panel2.add(textAreasp);
         panel2.add(Box.createRigidArea(new Dimension(20, 5)));
@@ -213,14 +229,20 @@ public class UI {
 
         //Panel3-----------------------------------------------------
 
+        //Initialize the third panel
+        //add the save button
         JPanel panel3 = new JPanel();
         panel3.add(save);
         panel3.add(Box.createRigidArea(new Dimension(50, 5)));
         panel3.add(clearButton);
         panel3.setLayout(new BoxLayout(panel3, BoxLayout.X_AXIS));
+        //set to center alignment
         panel3.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //and set background colour
         panel3.setBackground(backgroundC);
 
+        //add all the panels to the window
+        //and set spaces between each panel
         frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
         frame.getContentPane().add(Box.createRigidArea(new Dimension(5, 20)));
         frame.getContentPane().add(panel1);
@@ -236,41 +258,46 @@ public class UI {
     }
 
     /**
-     * @param text
-     * @param type
-     * @param needle
+     * @param text text to look up words from.
+     * @param type type of word to find.
+     * @param needle the word to find.
      * @param surroundingWords Number of words desired before & after the found result.
-     * @return
+     * @return result of the search
      */
     private List<POSResult> searchText(String text, SearchType type, String needle, Integer surroundingWords) {
         return search(text, type, needle, surroundingWords, surroundingWords);
     }
 
+    /**
+     * @param url url address to scrape text from
+     * @return
+     */
     private String getTxtFromURL(String url) throws IOException {
         return scrape(url);
     }
 
+    /**
+     * @param url rl address to scrape text from
+     * @param needle the word to find.
+     * @param type type of word to find.
+     * @param results information of the word(needle) in url
+     */
     public void save(String url, String needle, SearchType type, List<POSResult> results) {
         Saver saver = new Saver(url, needle, type, results);
         saver.save();
     }
-    
 
 
     /*
      * ActionListeners for the JButtons
      */
-
-
     private class SearchButtonHandler implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
-            // shows result
-
             String urlAddress = urlField.getText();
             String needle = wordField.getText();
             Integer surroundingWords = 0;
-            try {
+            try { //get surrounding words from the text box, if the given input is not an integer, show the message
                 surroundingWords = Integer.valueOf(surroundNfield.getText());
             } catch (NumberFormatException a){
                 a.printStackTrace();
@@ -280,7 +307,7 @@ public class UI {
             resultField.setText("\t \t  ----- Result ----- \t \t\n");
 
             SearchType type = null;
-            //"Exact form", "Lemma", "Entering tag"};
+            //set Type as selected item in the drop box
             switch (makeChoice.getSelectedItem().toString()) {
                 case "Exact form":
                     type = ExactForm;
@@ -293,45 +320,33 @@ public class UI {
                     break;
             }
 
-//            String text = "He crawls up, between my legs where he stops to rid me of my sodden panties. " +
-//                    "He slings it away carelessly, his ogling eyes never leaving the naked place they covered. " +
-//                    "He continues to stare, licking his lips – obviously beyond aroused by the sight but there’s nothing to hide my intimate folds and I feel exposed, squirming and certain that my blush reaches all the way down there.\n" +
-//                    "\n" +
-//                    "He takes his sweet, torturous time – luxuriating in his private viewing activity. " +
-//                    "He makes no move to touch me but the ravenous mould of his face is pushing me to run up the steps of desire, taking them three at a time. I shift in needy response.\n" +
-//                    "\n" +
-//                    "He growls, low in his chest while he grips my inner thighs, pushing them apart. " +
-//                    "“Keep still or I’ll make you.” I gasp at his provocative threat and on pure instinct and raw desire my hips tilt up by their own accord, crazy in its need for any contact. " +
-//                    "His hands slip around, cupping my backside as he pushes his nose into my sex, inhaling deeply.\n" +
-//                    "\n" +
-//                    "Oh my fucking my!";
             List<POSResult> results =null;
             String text = null;
 
 
-            try {
+            try { //check if the given url address is valid and if not, show the message
                 URL validatedUrl = new URL(urlAddress);
             } catch (MalformedURLException malformedURLException) {
                 malformedURLException.printStackTrace();
                 JOptionPane.showMessageDialog(frame, "Please enter a valid URL address");
                 return;
             }
-            if (needle.isEmpty()|| needle.contains(" ")) {
+            if (needle.isEmpty()|| needle.contains(" ")) {// if the given word is not valid, show the message
                 JOptionPane.showMessageDialog(frame, "Please enter a word");
                 return;
             }
 
-            if (surroundingWords <1) {
+            if (surroundingWords <1) { // if the given integer is smaller than 1, show the message
                 JOptionPane.showMessageDialog(frame, "Please enter the number of surrounding words (that is larger than 0)");
                 return;
             }
 
-            if(type == null){
+            if(type == null){ // if the type is not selected, show the message
                 JOptionPane.showMessageDialog(frame, "Please select a type");
                 return;
             }
 
-            try {
+            try { // try to load text from the url address
                 text = getTxtFromURL(urlAddress);
             } catch (IOException ioException) {
                 ioException.printStackTrace();
@@ -340,14 +355,14 @@ public class UI {
             results = searchText(text, type, needle, surroundingWords);
 
 
-            if (results.size() == 0) {
+            if (results.size() == 0) { //if there is no data in the result list, show message
                 JOptionPane.showMessageDialog(frame, "word not found");
             }
 
-            if (results.isEmpty()) {
+            if (results.isEmpty()) { //if there is no data in the result list, print "(0 result)"
                 sentenceField.append("  (0 result)\n");
                 resultField.append("  (0 result)\n");
-            } else {
+            } else { // if there is data, iterate the sentences and tokens in there and append it to the sentenceField
                 for (POSResult a : results) {
                     String sentence = "";
                     for (String token : a.getSentence()) {
@@ -355,12 +370,11 @@ public class UI {
 
                     }
                     sentenceField.append("  " + sentence + "\n");
-                    try {
+
+                    try { // look for the needle(word) from the sentenceField and save indexes to the List indexes
                         List<Integer> indexes = new ArrayList<>();
                         int index = 0;
                         while (index != -1) {
-                            //String textUppercase = sentenceField.getText().toLowerCase(Locale.ROOT);
-                           // index = textUppercase.indexOf(" " + needle.toLowerCase(Locale.ROOT) + " ", index);
                             String textUppercase = sentenceField.getText();
                             index = textUppercase.indexOf(" " + a.getWord() + " ", index);
                             if (index != -1) {
@@ -369,13 +383,14 @@ public class UI {
                             }
                         }
 
-                        for (int i : indexes) {
+                        for (int i : indexes) { //highlight all the words using the saved indexes
                             sentenceField.getHighlighter().addHighlight(i + 1, i + a.getWord().length() + 1, new DefaultHighlighter.DefaultHighlightPainter(Color.yellow));
                         }
 
                     } catch (BadLocationException badLocationException) {
                         badLocationException.printStackTrace();
                     }
+                    //print information to the resultField
                     resultField.append("  " + "Word: " + a.getWord() + ", Lemma: " + a.getLemma() + ", POSTag: " + a.getPos() + "\n");
 
                 }
@@ -393,7 +408,7 @@ public class UI {
             Integer surroundingWords = Integer.valueOf(surroundNfield.getText());
 
             SearchType type = null;
-            //"Exact form", "Lemma", "Entering tag"};
+            //set Type as selected item in the drop box
             switch (makeChoice.getSelectedItem().toString()) {
                 case "Exact form":
                     type = ExactForm;
@@ -418,8 +433,8 @@ public class UI {
 
             try {
                 save(urlAddress, needle, type, results);
-            } catch (NullPointerException en) {// if the window is closed and point null, catch error
-                JOptionPane.showMessageDialog(frame, "Saved");
+            } catch (NullPointerException en) {
+                JOptionPane.showMessageDialog(frame, "Not saved");
             }
 
         }
@@ -427,7 +442,7 @@ public class UI {
 
 
     private class ClearButtonHandler implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
+        public void actionPerformed(ActionEvent e) { //set every field as the initial setting
             wordField.setText("Enter a word");
             surroundNfield.setText("Enter the number of surrounding words");
             urlField.setText("Enter the URL");
@@ -437,7 +452,7 @@ public class UI {
         }
     }
 
-    private class ValueReporter implements ListSelectionListener {  ///????이거뭐임
+    private class ValueReporter implements ListSelectionListener {
 
         public void valueChanged(ListSelectionEvent event) {
         }
@@ -450,7 +465,7 @@ public class UI {
      */
     private class MyWindowListener extends WindowAdapter {
         public void windowClosing(WindowEvent e) {
-            //Disposes of the current window, and closes the
+            //Disposes of the current window
             frame.dispose();
         }
     }
